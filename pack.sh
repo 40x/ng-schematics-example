dc4141d7e1113d992aa72894d479612fede68613
Kashyap Mukkamala <kmukkamala@egen.solutions>
2018-12-11 15:18:17 -0600

    first commit

---
+++ pack.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ng build superhero-library
+
+echo "Building Schematics"
+
+cd projects/superhero-library
+
+tsc -p tsconfig.schematics.json
+
+echo "Copying collections to dist"
+
+cp collection.json ../../dist/superhero-library
+
+cd ../../dist/superhero-library
+
+npm publish
---
+++ projects/superhero-library/src/lib/superhero-library.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { SuperheroLibraryComponent } from './superhero-library.component';
+
+@NgModule({
+  declarations: [SuperheroLibraryComponent],
+  imports: [
+  ],
+  exports: [SuperheroLibraryComponent]
+})
+export class SuperheroLibraryModule { }
